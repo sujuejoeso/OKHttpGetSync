993fd229ad24c3c32ef5373f234f827d3f3e5118
Joe <tamatenoworries@yahoo.com>
2020-02-15 13:08:31 +1030

    Initial commit

---
+++ app/src/main/java/com/joeso/okhttptest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.joeso.okhttptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
